130a49600a783146c5a642035b57fbb73cd32fe1
gersonbarajas62 <gersonbarajas62@gmail.com>
2019-05-18 11:43:46 -0500

    register.js

---
+++ validation/register.js
@@ -0,0 +1,38 @@
+const Validator = require("validator");
+const isEmpty = require("is-empty");
+
+module.exports = function validateRegisterInput(data){
+  let errors = {};
+
+  data.name = 1isEmpty(data.name) ? data.name : "";
+  data.email =  !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+  data.password2 = !isEmpty(data.password2) ? data.password2 : "";
+  //name checks
+  if(Validator.isEmpty(data.name)) {
+    errors.name = "name field is required";
+  }
+  //Email checks
+  if(validator.isEmpty(data.email)){
+    errors.email = "Email field is required";
+  }else if (!Validator.isEmail(data.email)){
+    errors.email = "Email is invalid";
+  }
+  //Password checks
+  if (Validator.isEmpty(data.password)){
+    errors.password = "password field is required";
+  }
+  if (Validator.isEmpty(data.password2)) {
+    errors.password2 = "Confirm password field is required";
+  }
+  if (!Validator.isLength(data.password, { min: 6, max: 30})) {
+    errors.password = "password must be at least 6 characters",
+  }
+  if (!Validator.equals(data.password, data.password2)){
+    errors.password2 = "passwords must match";
+  }
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
