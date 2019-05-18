2c8f936b687b14d001fdf2af6f08a308bfd89f21
gersonbarajas62 <gersonbarajas62@gmail.com>
2019-05-18 12:45:21 -0500

    added post request to user.js for login

--- routes/api/users.js
+++ routes/api/users.js
@@ -41,3 +41,48 @@ router.post("/register", (req, res) => {
     });
   });
 });
+
+
+  router.post("/login", (req, res) => {
+    
+    const { errors, isValid } = validateRegisterInput(req.body);
+    
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({ email }).then(user => {
+      if (!user) {
+        return res.status(404).json({ emailnotfound: "Email not found" });
+      }
+      bcrypt.compare(password, user.password).then(isMatch => {
+        if (isMatch) {
+          const payload = {
+            id: user.id,
+            name: user.name
+          };
+
+          jwt.sign(
+            payload,
+            keys.secretOrKey,
+            {
+              expiresIn: 31556926
+            },
+            (err, token) => {
+              res.json({
+                sucess: true,
+                token: "Bearer " + token
+              });
+            }
+          );
+        }else {
+          return res
+          .status(400)
+            .json({ passwordincorrect: "Password incorrect" });
+        }
+      });
+    });
+  });
