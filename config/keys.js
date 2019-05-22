bab62773f194326cfdf6978cce8bbfb5228eed9a
gersonbarajas62 <gersonbarajas62@gmail.com>
2019-05-21 21:56:57 -0500

    change the config file to json file

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mongoURI: "mongodb+srv://gerson101:Holacode101@hiring-portal-huipr.mongodb.net/test?retryWrites=true",
-  secretOrKey: "secret"
-};
--- config/passport.js
+++ config/passport.js
@@ -1,4 +1,4 @@
-const JwtStrategy = require("passport-jwt").Strategy;
+/*const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const mongoose = require("mongoose");
 const User = mongoose.model("users");
@@ -22,4 +22,4 @@ module.exports = passport => {
       .catch(err => console.log(err))
     })
   );
-};
+};*/
