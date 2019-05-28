b8cb50cf53d478d6b5f9c13b76e5a50fce119081
gersonbarajas62 <gersonbarajas62@gmail.com>
2019-05-28 00:08:00 -0500

    fixing bugs

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const config = require('config');
+const jwt = require('jsonwebtoken')
 
 const app = express();
 
