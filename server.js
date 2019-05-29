3c5fc1d93032c9194e8051a529e51d2d5e93dfde
gersonbarajas62 <gersonbarajas62@gmail.com>
2019-05-29 12:31:28 -0500

    changes

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const path = require('path');
 const config = require('config');
 const jwt = require('jsonwebtoken')
-
 const app = express();
 
 // Bodyparser Middleware
